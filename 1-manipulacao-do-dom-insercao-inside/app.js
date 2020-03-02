b2f262de54a672ed498391fc9d7731664c83f879
Izabela <57100909+izavasconcelos@users.noreply.github.com>
2020-03-02 09:23:44 -0300

    dom insercao inside
    Comandos Aprendidos:
    - .append();
    - .appendTo();
    - .html();
    - .text();
    - .prepend();
    - .prependTo();

---
+++ 1-manipulacao-do-dom-insercao-inside/app.js
@@ -0,0 +1,27 @@
+/*
+Comandos Aprendidos:
+- .append();
+- .appendTo();
+- .html();
+- .text();
+- .prepend();
+- .prependTo();
+
+*/
+
+$('p').append(' Testendo append');
+
+$('p').append($('.introducao h1'));
+
+$('h2').appendTo($('.introducao h1'));
+
+var conteudoH2 = $('h2').html(); /* GET - Pega o primeiro h2 do site*/
+console.log(conteudoH2); /**/
+
+$('p').html('Teste'); /* Troca todos p por teste*/
+$('p').html('<span>Teste</span>'); 
+$('p').text('<span>Teste</span>'); 
+
+console.log($('h2').text()); /* Get text Pega todos h2*/
+
+$('h1').prepend('Prepend Teste');
